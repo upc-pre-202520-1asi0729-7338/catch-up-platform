e4d89eca24cae431cf98d27a0655d9614b68cba1
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-21 12:20:17 -0500

    feat(favorite-sources): implement command and query services for managing favorite sources.

---
+++ src/main/java/com/acme/catchup/platform/news/application/internal/queryservices/FavoriteSourceQueryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.acme.catchup.platform.news.application.internal.queryservices;
+
+import com.acme.catchup.platform.news.domain.model.aggregates.FavoriteSource;
+import com.acme.catchup.platform.news.domain.model.queries.GetAllFavoriteSourcesByNewsApiKeyQuery;
+import com.acme.catchup.platform.news.domain.model.queries.GetFavoriteSourceByIdQuery;
+import com.acme.catchup.platform.news.domain.model.queries.GetFavoriteSourceByNewsApiKeyAndSourceIdQuery;
+import com.acme.catchup.platform.news.domain.services.FavoriteSourceQueryService;
+import com.acme.catchup.platform.news.infrastructure.persistence.jpa.repositories.FavoriteSourceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FavoriteSourceQueryServiceImpl implements FavoriteSourceQueryService {
+    private final FavoriteSourceRepository favoriteSourceRepository;
+
+    public FavoriteSourceQueryServiceImpl(FavoriteSourceRepository favoriteSourceRepository) {
+        this.favoriteSourceRepository = favoriteSourceRepository;
+    }
+
+    @Override
+    public List<FavoriteSource> handle(GetAllFavoriteSourcesByNewsApiKeyQuery query) {
+        return favoriteSourceRepository.findAllByNewsApiKey(query.newsApiKey());
+    }
+
+    @Override
+    public Optional<FavoriteSource> handle(GetFavoriteSourceByIdQuery query) {
+        return favoriteSourceRepository.findById(query.id());
+    }
+
+    @Override
+    public Optional<FavoriteSource> handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query) {
+        return favoriteSourceRepository.findByNewsApiKeyAndSourceId(query.newsApiKey(), query.sourceId());
+    }
+}
